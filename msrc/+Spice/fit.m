fa6d7aaac0a8dee3bb7cd29ca878e9dec996952e
Ivan Ukhov <uvsoft@gmail.com>
2012-07-16 20:05:41 +0200

    Ngspice + Ruby + Matlab

---
+++ msrc/+Spice/fit.m
@@ -0,0 +1,55 @@
+function [ fresult, gof ] = fit(filename)
+  if nargin < 1, filename = Utils.resolvePath('nmos.out'); end
+
+  M = dlmread(filename, '\t', 1, 0);
+
+  L = M(:, 1);
+  T = Utils.toKelvin(M(:, 2));
+  I = M(:, 3);
+
+  Tonly = length(unique(L)) == 1;
+  Lonly = length(unique(T)) == 1;
+
+  if Lonly
+    ft = fittype('poly3');
+    opts = fitoptions(ft);
+    [ x, y ] = prepareCurveData(L, I);
+    vars = 4;
+  elseif Tonly
+    ft = fittype('poly3');
+    opts = fitoptions(ft);
+    [ x, y ] = prepareCurveData(T, I);
+    vars = 4;
+  else
+    ft = fittype('poly33');
+    opts = fitoptions(ft);
+    [ x, y, z ] = prepareSurfaceData(L, T, I);
+    vars = 10;
+  end
+
+  opts.Normalize = 'on';
+  opts.Lower = ones(1, vars) * -Inf;
+  opts.Upper = ones(1, vars) * Inf;
+
+  if Lonly
+    [ fitresult, gof ] = fit(x, y, ft, opts);
+    plot(fitresult, x, y);
+    xlabel('L');
+    ylabel('I');
+    legend('Original', 'Fitted');
+  elseif Tonly
+    [ fitresult, gof ] = fit(x, y, ft, opts);
+    plot(fitresult, x, y);
+    xlabel('T');
+    ylabel('I');
+    legend('Original', 'Fitted');
+  else
+    [ fitresult, gof ] = fit([ x, y ], z, ft, opts);
+    plot(fitresult, [ x, y ], z);
+    xlabel('L');
+    ylabel('T');
+    zlabel('I');
+    legend('Fitted', 'Original');
+  end
+  grid on;
+end
--- msrc/+Spice/run.rb
+++ msrc/+Spice/run.rb
@@ -4,41 +4,101 @@ require 'progressbar'
 
 netlist = 'nmos.cir'
 netlist_tmp = "#{ netlist }_tmp"
-netlist_out = File.basename(netlist, '.cir') + '.out'
+
+if ARGV.count > 2
+  netlist_out = ARGV[2]
+else
+  netlist_out = File.basename(netlist, '.cir') + '.out'
+end
 
 dir = File.dirname(__FILE__)
 
 netlist = File.open(File.join dir, netlist).read
 
-params = {
-  'Temp' => (27..100)
-}
+#
+# Channel length
+#
+
+if ARGV.count > 0
+  Lcount = ARGV[0].to_i
+else
+  Lcount = 100
+end
+
+Lnom = 22e-9
+Ldev = 0.05 * Lnom
+
+Lmin = Lnom - Ldev
+Lrange = 2 * Ldev
+
+if Lcount == 1
+  L = [ Lnom ]
+else
+  L = Array.new(Lcount)
+
+  (0...Lcount).to_a.each do |i|
+    L[i] = "%.4e" % (Lmin + i * Lrange / (Lcount - 1))
+  end
+end
+
+#
+# Temperature
+#
+
+if ARGV.count > 1
+  Tcount = ARGV[1].to_i
+else
+  Tcount = 100
+end
+
+Tnom = 27
+
+Tmin = Tnom
+Trange = 100 - Tmin
+
+if Tcount == 1
+  T = [ Tnom ]
+else
+  T = Array.new(Tcount)
+
+  (0...Tcount).to_a.each do |i|
+    T[i] = "%.2f" % (Tmin + i * Trange / (Tcount - 1))
+  end
+end
+
+#
+# Simulation
+#
 
 output = File.open(netlist_out, 'w')
 
-params.keys.each do |param|
-  pb = ProgressBar.new(param, params[param].count)
+output.puts "L\tT\tI"
 
-  output.puts "#{ param }\tValue"
+pb = ProgressBar.new('Ngspice', L.length * T.length);
 
-  params[param].each do |value|
-    netlist0 = netlist.gsub(/#{ param }\s*=\s*[^\s]+/, "#{ param } = #{ value }")
+L.each do |l|
+  T.each do |t|
+    netlist0 = netlist.gsub(/L\s*=\s*[^\s]+/, "L = #{ l }")
+    netlist0 = netlist0.gsub(/Temp\s*=\s*[^\s]+/, "Temp = #{ t }")
+
     File.open(File.join(dir, netlist_tmp), 'w') { |f| f.write netlist0 }
     pipe = IO.popen("cd #{ dir } && ngspice -b #{ netlist_tmp } 2> /dev/null")
     lines = pipe.readlines
+    pipe.close
+
     lines.each do |line|
       next unless line.match /@m1\[id\]\s*=\s*(.*)/
-      output.puts "#{ value }\t#{ $1 }"
+      output.puts "#{ l }\t#{ t }\t#{ $1 }"
       break
     end
+
     pb.inc
   end
-
-  output.puts
-  puts
 end
+
+puts
 
 output.close
 
-File.delete File.join(dir, netlist_tmp)
-File.delete File.join(dir, 'bsim4.out')
+File.delete File.join(dir, netlist_tmp) rescue true
+File.delete File.join(dir, 'bsim4.out') rescue true
