dc4209d2dba6d359c6f8c71cd079cddcd5b23f99
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-05-14 09:30:57 +0200

    Adjust a bit +Showcase/plotTraces

--- src/+Test/+Showcase/plotTraces.m
+++ src/+Test/+Showcase/plotTraces.m
@@ -1,18 +1,20 @@
 function plotTraces
-  options = Test.configure;
+  setup;
 
-  mc = Temperature.MonteCarlo.Transient(options.temperatureOptions);
+  options = Test.configure('processorCount', 4, 'powerScale', 1);
 
-  processors = [ 1, 2 ];
+  temperature = Temperature.Analytical.Transient(options.temperatureOptions);
 
-  Lnom = LeakagePower.Lnom;
-  scale = [ 1, 0.90, 0.85 ];
+  processors = [ 1 ];
+
+  Lnom = LeakagePower.Base.Lnom;
+  scale = [ 1, 0.95, 0.90 ];
 
   t = (0:(options.stepCount - 1)) * options.samplingInterval;
 
   figure;
   for i = 1:length(scale)
-    T = Utils.toCelsius(mc.compute( ...
+    T = Utils.toCelsius(temperature.compute( ...
       options.dynamicPower, 'L', scale(i) * Lnom));
     for j = processors
       line(t, T(j, :), 'Color', Color.pick(i), 'LineWidth', 1);
