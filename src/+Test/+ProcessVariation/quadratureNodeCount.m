3e879e1c687a51d1873f4c21bd7f650bb5346677
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-07-03 10:19:12 +0200

    Move +ProcessVariation to the library

--- src/+Test/+ProcessVariation/quadratureNodeCount.m
+++ src/+Test/+ProcessVariation/quadratureNodeCount.m
@@ -4,12 +4,12 @@ quadratureRule = { 'GaussHermite', 'GaussHermiteHW' };
 polynomialOrder = [ 1 2 3 4 5 ];
 processorCount  = [ 2 4 8 16 32 ];
 
-dimension = zeros(length(processorCount), 1);
+dimensionCount = zeros(length(processorCount), 1);
 
 for i = 1:length(processorCount)
   options = Test.configure('processorCount', processorCount(i));
   process = ProcessVariation.(options.processModel)(options.processOptions);
-  dimension(i) = process.dimensionCount;
+  dimensionCount(i) = process.dimensionCount;
 end
 
 for k = 1:(2 * length(quadratureRule))
@@ -26,7 +26,7 @@ for k = 1:(2 * length(quadratureRule))
 
   fprintf('%10s%10s', 'PC order', 'QD order');
   for i = 1:length(processorCount)
-    fprintf('%15s', sprintf('%d / %d', processorCount(i), dimension(i)));
+    fprintf('%15s', sprintf('%d / %d', processorCount(i), dimensionCount(i)));
   end
   fprintf('\n');
 
@@ -36,7 +36,7 @@ for k = 1:(2 * length(quadratureRule))
     fprintf('%10d%10d', polynomialOrder(i), order);
 
     for j = 1:length(processorCount)
-      quadrature = Quadrature('dimension', dimension(j), ...
+      quadrature = Quadrature('dimensionCount', dimensionCount(j), ...
         'order', order, 'ruleName', ruleName, 'method', method);
       fprintf('%15d', quadrature.nodeCount);
     end
