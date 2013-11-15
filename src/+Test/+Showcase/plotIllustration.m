a5821ae66480c624bad5f8595e9a5a62321a9655
Ivan Ukhov <ivan.ukhov@gmail.com>
2013-11-15 10:50:30 +0100

    Minor changes in Test/Showcase

--- src/+Test/+Showcase/plotIllustration.m
+++ src/+Test/+Showcase/plotIllustration.m
@@ -1,12 +1,12 @@
-function plotTraces
+function plotIllustration
   setup;
 
-  options = Test.configure('processorCount', 4);
+  options = Test.configure('processorCount', 2);
   display(options);
 
   temperature = Temperature.Analytical.Transient(options);
 
-  processors = 1:options.processorCount;
+  processors = 4;
 
   Vnom = LeakagePower.Base.Vnom;
   scale = [ 1.1, 0.95, 0.90 ];
@@ -16,7 +16,11 @@ function plotTraces
     T = Utils.toCelsius(temperature.compute(options.dynamicPower, ...
       'V', scale(i) * Vnom * ones(options.processorCount, 1)));
     for j = processors
-      Plot.line(options.timeLine, T(j, :), 'number', i, 'markEach', 18);
+      line(options.timeLine, T(j, :), 'Color', Color.pick(i), ...
+        'LineWidth', 2);
+      line(options.timeLine(1:18:end), T(j, 1:18:end), 'Color', Color.pick(i), ...
+        'LineStyle', 'None', 'LineWidth', 2, ...
+        'Marker', Marker.pick(i), 'MarkerSize', 16);
     end
   end
   Plot.label('Time, s', 'Temperature, C');
